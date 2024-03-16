8980b6ca84e787967559ef92ef1c508bb6abab12
Aaryanvarma22 <142662241+Aaryanvarma22@users.noreply.github.com>
2024-03-16 15:49:45 +0530

    Add files via upload

---
+++ atm project/atm_crt.py
@@ -0,0 +1,14 @@
+class atm_Start:
+    def display(self):
+        atm_bal=7500000
+        print("Welcome to GOTHAM Co-Operative Bank")
+        print("Enjoy your ATM Services")
+        print("The ATM's Limit is:",atm_bal)      
+#obj=atm_Start()
+#obj.display()
+
+        
+        
+    
+    
+
